27c810cdda5f726e4dab52feb7cbb7daf28ead03
sharque025 <sharque.toni@gmail.com>
2023-01-14 23:00:14 +0800

    Graph 2 code

---
+++ plot2.R
@@ -0,0 +1,21 @@
+#Import Data
+
+powerdata <- "household_power_consumption.txt"
+data <- read.table(powerdata, header = TRUE, sep = ";", dec= ".", na.strings = "?")
+head (data)
+
+#Subset dates
+powersub<- data[data$Date %in% c("1/2/2007","2/2/2007"), ]
+powersub
+
+
+#Format Date and Time
+datetime <- strptime(paste(powersub$Date, powersub$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+
+
+globalActivePower <- as.numeric(powersub$Global_active_power)
+
+#Creating the graph
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
